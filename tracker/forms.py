fc4a1b4106404dbc5be83700375513ee6cb70e90
Jake Wimberley <jakewimberley@users.noreply.github.com>
2016-07-04 20:46:48 -0400

    transition to base template and preliminary jquery-ification

--- tracker/forms.py
+++ tracker/forms.py
@@ -1,17 +1,17 @@
 from django import forms
 from functools import partial
 
-jqDateInput = forms.DateInput(attrs={'class':'datepicker'})
-jqTimeInput = forms.TimeInput(attrs={'class':'spinner'})
+jqDateInput = forms.DateInput(attrs={'class':'uiDatepicker'})
+jqTimeInput = forms.TimeInput() # TODO maybe not necessary
 
 class DiscussionForm(forms.Form):
     def __init__(self,*args,**kwargs):
       eventChoices = kwargs.pop('eventChoices')
       super(DiscussionForm,self).__init__(*args,**kwargs)
-      self.fields['_event'] = forms.MultipleChoiceField(label='select one or more events',choices=[(x.id, str(x)) for x in eventChoices])
-    _validDate = forms.DateField(label='valid date',widget=jqDateInput)
-    _validTime = forms.TimeField(label='valid time',widget=jqTimeInput)
-    _text = forms.CharField(label='discussion')
+      self.fields['_event'] = forms.MultipleChoiceField(label='select one or more events',choices=[(x.id, str(x)) for x in eventChoices],required=False)
+    _validDate = forms.DateField(label='valid date (UTC)',widget=jqDateInput)
+    _validTime = forms.TimeField(label='valid time (UTC)',widget=jqTimeInput,input_formats=['%H:%M','%H%M'])
+    _text = forms.CharField(label='discussion',widget=forms.Textarea)
 
 class DiscussionFormTextOnly(forms.Form):
-    _text = forms.CharField(label='discussion')
+    _text = forms.CharField(label='discussion',widget=forms.Textarea)
